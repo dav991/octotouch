eb4493bd0abc0d76f8643ac90e1160aaed33800d
David A <dav991@gmail.com>
2019-08-18 02:51:58 +0300

    Add forgoten files

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+
+rm -rf /opt/octotouch/
+mkdir -p /opt/octotouch
+mkdir -p /opt/octotouch/css
+mkdir -p /opt/octotouch/glade
+cp build/octotouch /opt/octotouch/
+cp build/css/* /opt/octotouch/css
+cp build/glade/* /opt/octotouch/glade
+cp octotouch.service /etc/systemd/system/
+systemctl enable octotouch
