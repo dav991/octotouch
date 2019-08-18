6aaba79b18b241c18079a2ef3c268346d78b14e8
David A <dav991@gmail.com>
2019-08-18 03:29:00 +0300

    Fix service and install.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ rm -rf /opt/octotouch/
 mkdir -p /opt/octotouch
 mkdir -p /opt/octotouch/css
 mkdir -p /opt/octotouch/glade
+cp build/octotouch.cfg /opt/octotouch/
 cp build/octotouch /opt/octotouch/
 cp build/css/* /opt/octotouch/css
 cp build/glade/* /opt/octotouch/glade
